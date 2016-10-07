07e90c5c47d7713cace2ffac43c3fa9007ef091c
qasir-11606016 <aliqasier@hotmail.com>
2016-10-08 00:50:47 +1100

    proper naming

--- ITC515_assignment2/src/datamanagement/UnitManager.java
+++ ITC515_assignment2/src/datamanagement/UnitManager.java
@@ -12,7 +12,7 @@ public class UnitManager {
 
 	private static UnitManager self = null;
 
-	private UnitMap UM;
+	private UnitMap unitMap_;
 
 	public static UnitManager unitManger() {
 		if (self == null)
@@ -21,11 +21,11 @@ public class UnitManager {
 	}
 
 	private UnitManager() {
-		UM = new UnitMap();
+		unitMap_ = new UnitMap();
 	}
 
 	public IUnit getUnit(String uc) {
-		IUnit iu = UM.get(uc);
+		IUnit iu = unitMap_.get(uc);
 		return iu != null ? iu : createUnit(uc);
 
 	}
@@ -55,7 +55,7 @@ public class UnitManager {
 						Integer.valueOf(el.getAttributeValue("examwgt"))
 						.intValue(), StudentUnitRecordManager
 						.instance().getRecordsByUnit(unitCode));
-				UM.put(iu.getUnitCode(), iu);
+				unitMap_.put(iu.getUnitCode(), iu);
 				return iu;
 			}
 
