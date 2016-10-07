ee002e1fb080b2ac7d1a44e14d18ab7255d259fd
qasir-11606016 <aliqasier@hotmail.com>
2016-10-08 00:28:21 +1100

    Function Named changes

--- ITC515_assignment2/src/datamanagement/ListUnitsCTL.java
+++ ITC515_assignment2/src/datamanagement/ListUnitsCTL.java
@@ -7,10 +7,11 @@ package datamanagement;
 public class ListUnitsCTL {
 	private UnitManager unitManger_;
 	public ListUnitsCTL() {
-		unitManger_ = UnitManager.UM();
+		unitManger_ = UnitManager.unitManger();
 	}
 	public void listUnits( IUnitLister lister ) {
-		lister.clearUnits();UnitMap units = unitManger_.getUnits();
+		lister.clearUnits();
+		UnitMap units = unitManger_.getUnits();
 		for (String s : units.keySet() )
 			lister.addUnit(units.get(s));
 		}
