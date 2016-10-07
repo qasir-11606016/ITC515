1984f5f1c59eb3c2bb4701c4dfcdfb8aeb158a00
qasir-11606016 <aliqasier@hotmail.com>
2016-10-08 00:06:38 +1100

    Private variables name changed

--- ITC515_assignment2/src/datamanagement/StudentUnitRecordProxy.java
+++ ITC515_assignment2/src/datamanagement/StudentUnitRecordProxy.java
@@ -6,40 +6,40 @@
 package datamanagement;
 public class StudentUnitRecordProxy implements IStudentUnitRecord {
 	private Integer studentID_;
-	private String unitCode;
+	private String unitCode_;
 	private StudentUnitRecordManager studentUnitRecordManager_;
 	public StudentUnitRecordProxy( Integer id, String code ) 
 	{
-		this.studentID_ = id;this.unitCode = code;
+		this.studentID_ = id;this.unitCode_ = code;
 		this.studentUnitRecordManager_ = StudentUnitRecordManager.instance();}
 		public Integer getStudentID() { 
 		return studentID_;
 		}
 		public String getUnitCode() { 
-		return unitCode; 
+		return unitCode_; 
 	}
 		public void setAsg1(float mark) {
-		studentUnitRecordManager_.getStudentUnitRecord( studentID_, unitCode ).setAsg1(mark);
+		studentUnitRecordManager_.getStudentUnitRecord( studentID_, unitCode_ ).setAsg1(mark);
 		}
 	public float getAsg1() 
 	{
-		return studentUnitRecordManager_.getStudentUnitRecord( studentID_, unitCode ).getAsg1();
+		return studentUnitRecordManager_.getStudentUnitRecord( studentID_, unitCode_ ).getAsg1();
 		}
 	public void setAsg2(float mark) { 
-		studentUnitRecordManager_.getStudentUnitRecord( studentID_, unitCode ).setAsg2(mark);
+		studentUnitRecordManager_.getStudentUnitRecord( studentID_, unitCode_ ).setAsg2(mark);
 		}
 	public float getAsg2() {
 		
-		return studentUnitRecordManager_.getStudentUnitRecord( studentID_, unitCode ).getAsg2();
+		return studentUnitRecordManager_.getStudentUnitRecord( studentID_, unitCode_ ).getAsg2();
 	}
 	public void setExam(float mark) {
-		studentUnitRecordManager_.getStudentUnitRecord( studentID_, unitCode ).setExam(mark);
+		studentUnitRecordManager_.getStudentUnitRecord( studentID_, unitCode_ ).setExam(mark);
 	}
 	public float getExam() 
 	{
-		return studentUnitRecordManager_.getStudentUnitRecord( studentID_, unitCode ).getExam();
+		return studentUnitRecordManager_.getStudentUnitRecord( studentID_, unitCode_ ).getExam();
 		}
 	public float getTotal() {
-		return studentUnitRecordManager_.getStudentUnitRecord( studentID_, unitCode ).getTotal();
+		return studentUnitRecordManager_.getStudentUnitRecord( studentID_, unitCode_ ).getTotal();
 		}
 	}
